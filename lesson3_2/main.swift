5275b105ffcd8d91a9889f014db918052aa37b3f
Bogdan Shtelmakh <shtelmakh.bogdan@gmail.com>
2016-11-09 00:49:08 +0200

    data updated

--- lesson3_2/main.swift
+++ lesson3_2/main.swift
@@ -8,32 +8,27 @@
 
 import Foundation
 
-class Countries {
-    var countries = [String: Int]()
+class Countrys {
+    var countrys = [String: Int]()
     init (){
         
     }
     func showCountryPopulation (countryName:String) -> Int{
-        return countries[countryName]!
+        return countrys[countryName]!
     }
     func addCountryPopulation (countryName:String, countryPopulation:Int) -> Bool {
-        self.countries[countryName] = countryPopulation
+        self.countrys[countryName] = countryPopulation
         return true
     }
-    func showAllCountries () -> Dictionary <String, Int> {
-        return self.countries
+    func showAllCountrys () -> Dictionary <String, Int> {
+        return self.countrys
     }
 }
 
-var countrie = Countries()
-print(countrie.addCountryPopulation(countryName: "Ukraina", countryPopulation: 228))
-print(countrie.addCountryPopulation(countryName: "Raseya", countryPopulation: 1488))
-print(countrie.showAllCountries())
-print(countrie.showCountryPopulation(countryName: "Ukraina"))
-while true {
-    if let countryName = readLine(){
-        continue
-    }
-    //print("Type your country: ")
-}
+var country = Countrys()
+print(country.addCountryPopulation(countryName: "Ukraine", countryPopulation: 42642434 ))
+print(country.addCountryPopulation(countryName: "Russia", countryPopulation: 14420000))
+print(country.showAllCountrys())
+print(country.showCountryPopulation(countryName: "Ukraine"))
+
 
